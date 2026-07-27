89fbbabc9ea3ba9539864e71c4f4cb798fb4eca8
Hilmi Rizadha <hilmi.rizadha@gmail.com>
2026-07-28 01:21:46 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
-const startBtn=document.getElementById("startBtn");
+import { db } from "./firebase.js";
 
-startBtn.onclick=()=>{
+import {
+    collection,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-    const name=document.getElementById("name").value.trim();
+const startBtn = document.getElementById("startBtn");
+
+startBtn.onclick = async () => {
+
+    const name = document.getElementById("name").value.trim();
 
     if(name===""){
 
-        alert("Masukkan nama terlebih dahulu");
+        alert("Masukkan nama");
 
         return;
 
     }
+
+    await addDoc(
+
+        collection(db,"participants"),
+
+        {
+
+            name:name,
+
+            luckyNumber:null,
+
+            createdAt:serverTimestamp()
+
+        }
+
+    );
 
     localStorage.setItem("participantName",name);
 
