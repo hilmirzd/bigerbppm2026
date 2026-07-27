1fc869bc2ac5ef754fb2af8311b2b5b510707bb7
Hilmi Rizadha <hilmi.rizadha@gmail.com>
2026-07-28 00:01:24 +0700

    admin.js

---
+++ js/admin.js
@@ -0,0 +1,10 @@
+const tableBody = document.getElementById("tableBody");
+
+tableBody.innerHTML = `
+<tr>
+    <td>1</td>
+    <td>Contoh Peserta</td>
+    <td>58</td>
+    <td>27 Juli 2026</td>
+</tr>
+`;
