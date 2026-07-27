d11889d2f076e74b7e89b95ef4f4b3e95e3be5a4
Hilmi Rizadha <hilmi.rizadha@gmail.com>
2026-07-28 00:53:33 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,62 @@
-const tableBody = document.getElementById("tableBody");
+import { db } from "./firebase.js";
 
-tableBody.innerHTML = `
+import {
+
+collection,
+onSnapshot
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const tableBody =
+document.getElementById("tableBody");
+
+const totalParticipant =
+document.getElementById("totalParticipant");
+
+const usedNumber =
+document.getElementById("usedNumber");
+
+const remainingNumber =
+document.getElementById("remainingNumber");
+
+onSnapshot(
+
+collection(db,"participants"),
+
+(snapshot)=>{
+
+tableBody.innerHTML="";
+
+let no=1;
+
+snapshot.forEach(doc=>{
+
+const data=doc.data();
+
+tableBody.innerHTML+=`
+
 <tr>
-    <td>1</td>
-    <td>Contoh Peserta</td>
-    <td>58</td>
-    <td>27 Juli 2026</td>
+
+<td>${no++}</td>
+
+<td>${data.name}</td>
+
+<td>${data.luckyNumber}</td>
+
+<td>${new Date(data.createdAt.seconds*1000).toLocaleString()}</td>
+
 </tr>
+
 `;
+
+});
+
+totalParticipant.innerHTML=snapshot.size;
+
+usedNumber.innerHTML=snapshot.size;
+
+remainingNumber.innerHTML=100-snapshot.size;
+
+}
+
+);
