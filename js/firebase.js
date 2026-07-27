628901807aa5f463f8dccd22a7336ce5a7a61e02
Hilmi Rizadha <hilmi.rizadha@gmail.com>
2026-07-28 00:51:56 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,17 @@
-// Import the functions you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Ganti dengan konfigurasi milikmu
 const firebaseConfig = {
-
+    apiKey: "AIzaSyDdlFtRGf0xkZMYLbU_YncTfMTniazxo0Y",
+    authDomain: "biger-bppm-2026.firebaseapp.com",
+    projectId: "biger-bppm-2026",
+    storageBucket: "biger-bppm-2026.firebasestorage.app",
+    messagingSenderId: "57601958744",
+    appId: "1:57601958744:web:ba933e679e10a6589d7b2d",
+    measurementId: "G-Y3E58GC4QY"
 };
 
 const app = initializeApp(firebaseConfig);
