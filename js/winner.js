3575362add601355ee58a36b404e07ad01418d73
Hilmi Rizadha <hilmi.rizadha@gmail.com>
2026-07-28 23:02:34 +0700

    Update winner.js

--- js/winner.js
+++ js/winner.js
@@ -1,112 +1,111 @@
 import { db } from "./firebase.js";
 
 import {
-collection,
-getDocs,
-addDoc
+    collection,
+    getDocs,
+    addDoc
 }
-from 
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+const btn = document.getElementById("drawWinner");
+
+
+btn.addEventListener("click", async()=>{
+
+
+    // Ambil semua peserta
+    const pesertaSnap = await getDocs(
+        collection(db,"participants")
+    );
+
+
+    let peserta = [];
+
+
+    pesertaSnap.forEach((doc)=>{
+
+        peserta.push({
+            id:doc.id,
+            ...doc.data()
+        });
+
+    });
 
 
 
-const btn =
-document.getElementById("drawWinner");
+    // Ambil daftar pemenang sebelumnya
+    const winnerSnap = await getDocs(
+        collection(db,"winners")
+    );
+
+
+    let winners = [];
+
+
+    winnerSnap.forEach((doc)=>{
+
+        winners.push(
+            doc.data().nama
+        );
+
+    });
 
 
 
-btn.addEventListener(
-"click",
-async()=>{
-
-
-const pesertaSnap =
-await getDocs(
-collection(db,"participants")
-);
+    // Filter peserta yang belum pernah menang
+    let available = peserta.filter(
+        p => !winners.includes(p.nama)
+    );
 
 
 
-let peserta=[];
+    if(available.length === 0){
 
+        document.getElementById("winnerName").innerHTML =
+        "SEMUA PESERTA SUDAH MENANG";
 
-pesertaSnap.forEach(doc=>{
+        return;
 
-peserta.push({
-id:doc.id,
-...doc.data()
-});
-
-});
+    }
 
 
 
-const winnerSnap =
-await getDocs(
-collection(db,"winners")
-);
+    // Random pemenang
+    let winner =
+    available[
+        Math.floor(
+            Math.random()*available.length
+        )
+    ];
 
 
 
-let winners=[];
-
-
-winnerSnap.forEach(doc=>{
-
-winners.push(
-doc.data().name
-);
-
-});
-
-
-
-let available =
-peserta.filter(
-p=>!winners.includes(p.name)
-);
+    // Tampilkan pemenang
+    document.getElementById("winnerName").innerHTML =
+    `
+    ${winner.nama}
+    <br>
+    <small>
+    Lucky Number: ${winner.luckyNumber}
+    </small>
+    `;
 
 
 
-if(available.length===0){
+    // Simpan ke Firebase
+    await addDoc(
+        collection(db,"winners"),
+        {
 
-document.getElementById("winnerName")
-.innerHTML="SEMUA SUDAH MENANG";
+            nama:winner.nama,
 
-return;
+            luckyNumber:winner.luckyNumber,
 
-}
+            waktu:new Date()
 
-
-
-let winner =
-available[
-Math.floor(
-Math.random()*available.length
-)
-];
-
-
-
-document.getElementById("winnerName")
-.innerHTML=
-winner.name;
-
-
-
-await addDoc(
-collection(db,"winners"),
-{
-
-name:winner.name,
-
-number:winner.number,
-
-time:new Date()
-
-}
-
-);
+        }
+    );
 
 
 });
